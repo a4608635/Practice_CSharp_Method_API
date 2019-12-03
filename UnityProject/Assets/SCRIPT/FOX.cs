caa72aea694f8616b0f77dd39ae7907d2b55b7d5
a4608635 <57396749+a4608635@users.noreply.github.com>
2019-12-03 21:33:47 +0800

    作業還沒好阿!

---
+++ UnityProject/Assets/SCRIPT/FOX.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class FOX : MonoBehaviour
+{
+    [Header("速度"), Range(1, 100)]
+    public int aaa = 5;
+    [Header("剛體")]
+    public Rigidbody2D r2d;
+    [Header("圖片渲染器")]
+    public SpriteRenderer SSS;
+
+    private void AAA()
+    {
+        print( Input.GetAxisRaw("Horizontal") * Time.deltaTime);
+
+
+    }
+
+    private void Update()
+    {
+        AAA();
+       
+            
+
+    }
+}
